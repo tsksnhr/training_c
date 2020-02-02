d1220c775274de5c4125d7c5b78292de6de4a8bb
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-02-02 23:35:22 +0900

    edited file
    to solve question

--- prac_6_3_3.c
+++ prac_6_3_3.c
@@ -3,22 +3,32 @@
 
 int main(void){
 
-    char str[80];
+    char str[80], str_alt[80];
     char *pstr;
-    int length, i;
+    int length, i, flg;
 
     printf("type the string.\n");
     gets(str);
 
     length = strlen(str);
+    flg = 0;
 
-    pstr = str;
-
-    while(str[i]){
+    for (i=0; ((i<length) && (flg == 0)); i++){
         if (str[i] == ' '){
-            pstr = &str[i];
+            pstr = &str[i+1];
+            flg = 1;
         }
     }
+
+    i = 0;
+
+    while(*pstr){
+        str_alt[i] = *pstr++;
+        i++;
+    }
+    str_alt[i] = '\0';
+
+    printf(str_alt);
 
     return 0;
 }
